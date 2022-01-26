4dc0fd550d2c240b0addc12127a65d66b69ab250
Chuston13 <Cdhprogrammer@gmail.com>
2022-01-26 11:29:10 -0600

    index.html

---
+++ js.js
@@ -0,0 +1,26 @@
+const buttons=document.querySelectorAll('Parent')
+
+
+
+
+function computerPlay(){
+    const computerSelection = ['Rock','Paper','Scissors']
+   return Math.floor(math.random()) * computerSelection.length;
+    
+}
+function playRound(){
+    let playerScore=0
+    let computerScore=0
+    let result=''
+    if ((playerSelection=='Rock' && computerSelection =='Scissors' || playerSelection=='Paper' && computerSelection=='Rock'|| playerSelection=='scissors' && computerSelection=='paper'))
+  
+  
+        playerScore+=1
+        result=('Come Forth my Champion'+ playerSelection + 'beats' + computerSelection + "<br><br>Player score:"+playerScore+ '<br>computerScore:</br>'+ computerScore);
+
+
+
+
+
+   const playerSelection='Rock, Paper, Scissors';}
+    const computerSelection=computerPlay();
